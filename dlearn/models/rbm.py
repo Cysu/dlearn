8a013eeb5a2cd91c8996257007d36bd897436e28
Cysu <st.cysu@gmail.com>
2014-05-20 11:09:39 +0800

    Sketch RBM

---
+++ dlearn/models/rbm.py
@@ -0,0 +1,139 @@
+import numpy as np
+import theano
+import theano.tensor as T
+
+from ..utils import actfuncs, costfuncs
+from ..utils.math import nprng, tnrng
+
+
+class RBM(object):
+
+    def __init__(self, input, vshape, hshape,
+                 W=None, vbias=None, hbias=None):
+        super(RBM, self).__init__()
+
+        self._input = input
+
+        if isinstance(vshape, int):
+            self._vshape = vshape
+        elif isinstance(vshape, tuple) or isinstance(vshape, list):
+            self._vshape = np.prod(vshape)
+        else:
+            raise ValueError("vshape type error")
+
+        if isinstance(hshape, int):
+            self._hshape = hshape
+        elif isinstance(hshape, tuple) or isinstance(hshape, list):
+            self._hshape = np.prod(hshape)
+        else:
+            raise ValueError("hshape type error")
+
+        # Initialize parameters
+        if W is None:
+            W_bound = np.sqrt(6.0 / (self._vshape + self._hshape))
+            W_bound *= 4
+
+            init_W = np.asarray(
+                nprng.uniform(low=-W_bound, high=W_bound,
+                              size=(self._vshape, self._hshape)),
+                dtype=theano.config.floatX)
+
+            self._W = theano.shared(value=init_W, borrow=True)
+        else:
+            self._W = W
+
+        if vbias is None:
+            init_vbias = np.zeros(
+                self._vshape, dtype=theano.config.floatX)
+
+            self._vbias = theano.shared(value=init_vbias, borrow=True)
+
+        if hbias is None:
+            init_hbias = np.zeros(
+                self._hshape, dtype=theano.config.floatX)
+
+            self._hbias = theano.shared(value=init_hbias, borrow=True)
+
+        self._params = [self._W, self._vbias, self._hbias]
+
+    def free_energy(self, v_sample):
+        z = T.dot(v_sample, self._W) + self._hbias
+        vbias_term = T.dot(v_sample, self._vbias)
+        hidden_term = T.log(1 + T.exp(z)).sum(axis=1)
+        return -hidden_term - vbias_term
+
+    def sample_h_given_v(self, v0_sample):
+        pre_sigmoid_h1 = T.dot(v0_sample, self._W) + self._hbias
+        h1_mean = actfuncs.sigmoid(pre_sigmoid_h1)
+        h1_sample = tnrng.binomial(size=h1_mean.shape, n=1, p=h1_mean,
+                                   dtype=theano.config.floatX)
+        return [pre_sigmoid_h1, h1_mean, h1_sample]
+
+    def sample_v_given_h(self, h0_sample):
+        pre_sigmoid_v1 = T.dot(h0_sample, self._W.T) + self._vbias
+        v1_mean = actfuncs.sigmoid(pre_sigmoid_v1)
+        v1_sample = tnrng.binomial(size=v1_mean.shape, n=1, p=v1_mean,
+                                   dtype=theano.config.floatX)
+        return [pre_sigmoid_v1, v1_mean, v1_sample]
+
+    def gibbs_vhv(self, v0_sample):
+        pre_sigmoid_h1, h1_mean, h1_sample = self.sample_h_given_v(v0_sample)
+        pre_sigmoid_v1, v1_mean, v1_sample = self.sample_v_given_h(h1_sample)
+        return [pre_sigmoid_h1, h1_mean, h1_sample,
+                pre_sigmoid_v1, v1_mean, v1_sample]
+
+    def gibbs_hvh(self, h0_sample):
+        pre_sigmoid_v1, v1_mean, v1_sample = self.sample_v_given_h(h0_sample)
+        pre_sigmoid_h1, h1_mean, h1_sample = self.sample_h_given_v(v1_sample)
+        return [pre_sigmoid_v1, v1_mean, v1_sample,
+                pre_sigmoid_h1, h1_mean, h1_sample]
+
+    def get_cost_updates(self, lr=1e-3, persistent=None, k=1):
+        if persistent is None:
+            __, __, chain_start = self.sample_h_given_v(self._input)
+        else:
+            chain_start = persistent
+
+        init_outputs = [None] * 5 + [chain_start]
+
+        [pre_sigmoid_nvs, nv_means, nv_samples,
+         pre_sigmoid_nhs, nh_means, nh_samples], updates = \
+            theano.scan(self.gibbs_hvh, outputs_info=init_outputs, n_steps=k)
+
+        chain_end = nv_samples[-1]
+
+        cost = self.free_energy(self.input).mean() - \
+            self.free_energy(chain_end).mean()
+
+        grads = T.grad(cost, self._params, consider_constant=[chain_end])
+        for p, g in zip(self._params, grads):
+            updates[p] = p - g * T.cast(lr, dtype=theano.config.floatX)
+
+        if persistent:
+            updates[persistent] = nh_samples[-1]
+            monitoring_cost = self.get_pseudo_likelihood_cost(updates)
+        else:
+            monitoring_cost = self.get_reconstruction_cost(updates,
+                                                           pre_sigmoid_nvs[-1])
+
+        return monitoring_cost, updates
+
+    def get_pseudo_likelihood_cost(self, updates):
+        bit_i_idx = theano.shared(value=0)
+
+        xi = T.round(self.input)
+        fe_xi = self.free_energy(xi)
+
+        xi_flip = T.set_subtensor(xi[:, bit_i_idx], 1 - xi[:, bit_i_idx])
+        fe_xi_flip = self.free_energy(xi_flip)
+
+        cost = T.mean(self.n_visible * T.log(T.nnet.sigmoid(fe_xi_flip -
+                                                            fe_xi)))
+
+        updates[bit_i_idx] = (bit_i_idx + 1) % self.n_visible
+
+        return cost
+
+    def get_reconstruction_cost(self, updates, pre_sigmoid_nv):
+        y = actfuncs.sigmoid(pre_sigmoid_nv)
+        return costfuncs.binxent(y, self._input)
---
+++ examples/mnist_rbm.py
@@ -0,0 +1,136 @@
+import os
+import sys
+import cPickle
+import time
+import numpy as np
+import theano
+import theano.tensor as T
+
+homepath = '..'
+if not homepath in sys.path:
+    sys.path.insert(0, homepath)
+
+from dlearn.data.dataset import Dataset
+from dlearn.models.rbm import RBM
+from dlearn.utils import Wrapper
+from dlearn.utils.math import nprng
+from dlearn.visualization import show_channels
+
+
+def load_data():
+    fpath = os.path.join('..', 'data', 'mnist', 'mnist.pkl')
+
+    with open(fpath, 'rb') as f:
+        train_set, valid_set, test_set = cPickle.load(f)
+
+    dataset = Dataset(
+        train=Wrapper(
+            input=train_set[0],
+            target=train_set[1]
+        ),
+        valid=Wrapper(
+            input=valid_set[0],
+            target=valid_set[1]
+        ),
+        test=Wrapper(
+            input=test_set[0],
+            target=test_set[1]
+        ),
+        limit=None
+    )
+
+    return dataset
+
+
+def train_model(dataset):
+    # Construct the RBM model
+    X = T.matrix()
+    index = T.lvector()
+
+    n_epochs = 15
+    batch_size = 20
+    vshape, hshape = 784, 500
+    n_batches = dataset.train.cpu_data.shape[0] / batch_size
+
+    persistent_chain = theano.shared(np.zeros((batch_size, hshape),
+                                              dtype=theano.config.floatX),
+                                     borrow=True)
+
+    rbm = RBM(X, vshape, hshape)
+
+    cost, updates = rbm.get_cost_updates(lr=1e-3,
+                                         persistent=persistent_chain,
+                                         k=15)
+
+    # Train the model
+    if not os.path.isdir('rbm_plots'):
+        os.mkdir('rbm_plots')
+
+    train_data = dataset.train.cpu_data.astype(theano.config.floatX)
+    train_data = theano.shared(train_data, borrow=True)
+
+    train_rbm = theano.function(
+        inputs=[index], outputs=cost, updates=updates,
+        givens={X: train_data[index * batch_size: (index + 1) * batch_size]})
+
+    plot_time = 0
+    start_time = time.clock()
+
+    for epoch in n_epochs:
+        mean_cost = np.mean([train_rbm(i) for i in n_batches])
+
+        print 'Training epoch {0}, cost {1}'.format(epoch, mean_cost)
+
+        plot_start = time.clock()
+        W = rbm._W.get_value(borrow=True).T[1:100]
+        ofpath = os.path.join(
+            'rbm_plots', 'filter_epoch_{:03d}.png'.format(epoch))
+        show_channels(W, n_cols=10, ofpath=ofpath)
+        plot_stop = time.clock()
+        plot_time += plot_stop - plot_start
+
+    stop_time = time.clock()
+
+    print 'Training finished, time {0}'.format(
+        stop_time - start_time - plot_time)
+
+
+def save_model(model):
+    with open('model.pkl', 'wb') as f:
+        cPickle.dump(model, f, cPickle.HIGHEST_PROTOCOL)
+
+
+def gen_samples(dataset, model):
+    n_samples = 10
+    n_chains = 20
+    plot_every = 1000
+
+    test_idx = nprng.randint(dataset.test.cpu_data.shape[0] - n_chains)
+
+    persistent_chain = theano.shared(
+        dataset.test.cpu_data[test_idx: test_idx + n_chains].astype(
+            theano.config.floatX),
+        borrow=True)
+
+    [pre_sigmoid_hs, h_means, h_samples,
+     pre_sigmoid_vs, v_means, v_samples], updates = \
+        theano.scan(model.gibbs_vhv,
+                    outputs_info=[None] * 5 + [persistent_chain],
+                    n_step=plot_every)
+
+    updates.update({persistent_chain: v_samples[-1]})
+
+    sample_fn = theano.function(
+        inputs=[], outputs=v_samples[-1], updates=updates)
+
+    evolutions = np.vstack([sample_fn() for __ in xrange(n_samples)])
+
+    show_channels(evolutions, n_cols=n_chains, normalize=[0, 1],
+                  ofpath=os.path.join('rbm_plots', 'samples.png'))
+
+
+if __name__ == '__main__':
+    dataset = load_data()
+    model = train_model(dataset)
+    save_model(model)
+    gen_samples(dataset, model)
